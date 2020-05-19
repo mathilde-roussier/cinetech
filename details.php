0781338e212e8f48e48d6e6055c3c13c5b43fc89
Mathilde <mathilde.roussier@laplateforme.io>
2020-05-19 11:34:48 +0200

    recupération des commentaires de l'API pour les films + Ajout input ajout de commentaires

--- details.php
+++ details.php
@@ -42,77 +42,92 @@ foreach ($_GET as $champ => $info) {
 
     <main class="container">
 
-        <div class="media">
-            <?php if ($data_detail_decode['poster_path'] != null) { ?>
-                <img src="http://image.tmdb.org/t/p/w500<?php echo $data_detail_decode['poster_path']; ?>" class="mr-3" alt="...">
-            <?php } else { ?>
-                <img src="assets/no_img.jpg" class="mr-3" alt="...">
-            <?php } ?>
+        <section>
 
-            <div class="media-body">
-                <div class="d-flex justify-content-between">
+            <div class="media">
+                <?php if ($data_detail_decode['poster_path'] != null) { ?>
+                    <img src="http://image.tmdb.org/t/p/w500<?php echo $data_detail_decode['poster_path']; ?>" class="mr-3" alt="...">
+                <?php } else { ?>
+                    <img src="assets/no_img.jpg" class="mr-3" alt="...">
+                <?php } ?>
+
+                <div class="media-body">
+                    <div class="d-flex justify-content-between">
+                        <?php if ($champ == 'id_film') { ?>
+                            <h5 class="mt-0"><?php echo $data_detail_decode['title']; ?></h5>
+                        <?php } else { ?>
+                            <h5 class="mt-0"><?php echo $data_detail_decode['name']; ?></h5>
+                        <?php } ?>
+                        <aside><?php echo $data_detail_decode['vote_average']; ?> / 10</aside>
+                    </div>
+                    <p><?php echo $data_detail_decode['overview']; ?></p>
                     <?php if ($champ == 'id_film') { ?>
-                        <h5 class="mt-0"><?php echo $data_detail_decode['title']; ?></h5>
+                        <p><?php echo $data_detail_decode['runtime'] . " min"; ?></p>
                     <?php } else { ?>
-                        <h5 class="mt-0"><?php echo $data_detail_decode['name']; ?></h5>
+                        <p><?php echo $data_detail_decode['episode_run_time'][0] . " min"; ?></p>
                     <?php } ?>
-                    <aside><?php echo $data_detail_decode['vote_average']; ?> / 10</aside>
-                </div>
-                <p><?php echo $data_detail_decode['overview']; ?></p>
-                <?php if ($champ == 'id_film') { ?>
-                    <p><?php echo $data_detail_decode['runtime'] . " min"; ?></p>
-                <?php } else { ?>
-                    <p><?php echo $data_detail_decode['episode_run_time'][0] . " min"; ?></p>
-                <?php } ?>
-                <p><?php foreach ($data_detail_decode['genres'] as $genres) {
-                        echo get_object_vars($genres)['name'] . " | ";
-                    } ?></p>
-                <?php if ($champ == 'id_film') { ?>
-                    <p><?php echo $data_detail_decode['release_date']; ?></p>
-                <?php } else { ?>
-                    <p><?php echo $data_detail_decode['first_air_date'] . " | " . $data_detail_decode['last_air_date']; ?></p>
-                    <p>Season(s) : <?php echo $data_detail_decode['number_of_seasons']; ?></p>
-                    <p>Episode(s) : <?php echo $data_detail_decode['number_of_episodes']; ?></p>
-                <?php } ?>
+                    <p><?php foreach ($data_detail_decode['genres'] as $genres) {
+                            echo get_object_vars($genres)['name'] . " | ";
+                        } ?></p>
+                    <?php if ($champ == 'id_film') { ?>
+                        <p><?php echo $data_detail_decode['release_date']; ?></p>
+                    <?php } else { ?>
+                        <p><?php echo $data_detail_decode['first_air_date'] . " | " . $data_detail_decode['last_air_date']; ?></p>
+                        <p>Season(s) : <?php echo $data_detail_decode['number_of_seasons']; ?></p>
+                        <p>Episode(s) : <?php echo $data_detail_decode['number_of_episodes']; ?></p>
+                    <?php } ?>
 
-                <p>Stars : <?php if (count($data_credit_decode['cast']) > 2) {
-                                for ($i = 0; $i <= 2; $i++) {
-                                    echo get_object_vars($data_credit_decode['cast'][$i])['name'] . " , ";
-                                }
-                            ?>
-                        etc..</p>
-            <?php
-                            } elseif (empty($data_credit_decode['cast'])) { ?>
-                non communiquées.</p>
-            <?php } else {
-                                foreach ($data_credit_decode['cast'] as $cast) {
-                                    echo get_object_vars($cast)['name'] . " , ";
+                    <p>Stars : <?php if (count($data_credit_decode['cast']) > 2) {
+                                    for ($i = 0; $i <= 2; $i++) {
+                                        echo get_object_vars($data_credit_decode['cast'][$i])['name'] . " , ";
+                                    }
+                                ?>
+                            etc..</p>
+                <?php
+                                } elseif (empty($data_credit_decode['cast'])) { ?>
+                    non communiquées.</p>
+                <?php } else {
+                                    foreach ($data_credit_decode['cast'] as $cast) {
+                                        echo get_object_vars($cast)['name'] . " , ";
+                                    } ?>
+                    etc..</p>
+                <?php
+                                    // echo get_object_vars($data_credit_decode['cast'][0])['name'] . " , ";
                                 } ?>
-                etc..</p>
-            <?php
-                                // echo get_object_vars($data_credit_decode['cast'][0])['name'] . " , ";
-                            } ?>
-            <?php if ($champ == 'id_film') { ?>
-                <p>Director(s) : <?php foreach ($data_credit_decode['crew'] as $crew) {
-                                        if (get_object_vars($crew)['job'] == 'Director') {
-                                            echo get_object_vars($crew)['name'] . " , ";
-                                        }
-                                    } ?></p>
-            <?php } else { ?>
-                <p>Creator(s) : <?php foreach ($data_detail_decode['created_by'] as $creator) {
-                                    echo get_object_vars($creator)['name'] . " , ";
-                                }
-                            } ?></p>
+                <?php if ($champ == 'id_film') { ?>
+                    <p>Director(s) : <?php foreach ($data_credit_decode['crew'] as $crew) {
+                                            if (get_object_vars($crew)['job'] == 'Director') {
+                                                echo get_object_vars($crew)['name'] . " , ";
+                                            }
+                                        } ?></p>
+                <?php } else { ?>
+                    <p>Creator(s) : <?php foreach ($data_detail_decode['created_by'] as $creator) {
+                                        echo get_object_vars($creator)['name'] . " , ";
+                                    }
+                                } ?></p>
 
-                <?php if (isset($_SESSION['id'])) {
-                    $bdd->checkfav($info);
-                } else { ?>
-                    <a href="connexion.php" class="btn btn-primary">Ajouter aux favoris</a>
+                    <?php if (isset($_SESSION['id'])) {
+                        $bdd->checkfav($info);
+                    } else { ?>
+                        <a href="connexion.php" class="btn btn-primary">Ajouter aux favoris</a>
 
-                <?php } ?>
+                    <?php } ?>
 
+                </div>
             </div>
-        </div>
+        </section>
+
+        <!-- Espace Commentaire -->
+
+        <section>
+            <div id="addcomment" class="d-flex col-3 justify-content-between">
+                <textarea placeholder='Commentaire...'></textarea>
+                <button class="btn btn-primary">Valider</button>
+            </div>
+
+            <?php var_dump($data_reviews_decode); ?>
+
+        </section>
 
     </main>
 
--- include/requete_film.php
+++ include/requete_film.php
@@ -1,7 +1,5 @@
 <?php
 
-// URL de base http://api.themoviedb.org/3/discover/movie?api_key=21265361ae3ee1f790c63a3a2973a4f2&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=16&with_original_language=fr
-
 $cle = "21265361ae3ee1f790c63a3a2973a4f2";
 
 // create curl resource
@@ -24,7 +22,6 @@ if (empty($_GET)) {
     curl_close($ch);
 
     $data = JSON_decode($output);
-    // var_dump($data);
 
     $data_decode = get_object_vars($data);
 } else {
@@ -33,12 +30,11 @@ if (empty($_GET)) {
         if ($champ == 'page') {
             // set url
             $newurl = $url . '&' . $champ . "=" . $info;
-            // echo $newurl;
         } elseif ($champ == 'id_film') {
             // set url
             $newurl = "http://api.themoviedb.org/3/movie/" . $info . "?api_key=21265361ae3ee1f790c63a3a2973a4f2&language=fr-FR";
             $newurl_credit = "http://api.themoviedb.org/3/movie/" . $info . "/credits?api_key=21265361ae3ee1f790c63a3a2973a4f2";
-            // echo $newurl;
+            $url_reviews = "http://api.themoviedb.org/3/movie/" . $info . "/reviews?api_key=21265361ae3ee1f790c63a3a2973a4f2";
         }
 
         curl_setopt($ch, CURLOPT_URL, $newurl);
@@ -60,14 +56,27 @@ if (empty($_GET)) {
             // $output contains the output string
             $output_credit = curl_exec($ch);
 
-            $data_credit= JSON_decode($output_credit);
-            // var_dump($data);
+            $data_credit = JSON_decode($output_credit);
 
             $data_credit_decode = get_object_vars($data_credit);
+
+
+            // REVIEWS 
+
+            curl_setopt($ch, CURLOPT_URL, $url_reviews);
+
+            //return the transfer as a string
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+            // $output contains the output string
+            $output_reviews = curl_exec($ch);
+
+            $data_reviews = JSON_decode($output_reviews);
+
+            $data_reviews_decode = get_object_vars($data_reviews);
         }
 
         $data_detail = JSON_decode($output);
-        // var_dump($data);
 
         $data_detail_decode = get_object_vars($data_detail);
     }
